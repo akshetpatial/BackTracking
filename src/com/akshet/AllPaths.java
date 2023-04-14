40b1df5cc1caad61c1331e0e510b1acbcb5e037b
AKSHET PATIAL <akshet.patial@gmail.com>
2023-04-14 13:01:44 +0530

    Made minor changes

--- src/com/akshet/AllPaths.java
+++ src/com/akshet/AllPaths.java
@@ -15,12 +15,14 @@ public class AllPaths {
                 bool[i][j]=true;
             }
         }
+        int count=1;
+        int[][] path = new int[row][col];
 
         // Extra row and col just for the 2D bool array to iterate
-       return helperFunction(row,col,bool,"",row,col);
+       return helperFunction(row,col,bool,"",row,col,count,path);
     }
 
-    public static ArrayList<String> helperFunction(int row, int col, Boolean[][] bool, String s, int rowB ,int colB) {
+    public static ArrayList<String> helperFunction(int row, int col, Boolean[][] bool, String s, int rowB ,int colB, int count, int[][] path) {
 
         // If we Run the below code it will run infinite time as steps will get repeated and this will stack overflow
 
@@ -31,15 +33,26 @@ public class AllPaths {
         // Here By using Backtracking we can solve this in simple terms reverting back the change when the call comes out
         // of the recursion call
         ArrayList<String> check = new ArrayList<>();
+
         if(row==1 && col==1){
+            path[rowB-row][colB-col]=count;
+           for(int[] element : path)
+               System.out.println(Arrays.toString(element));
+            System.out.print("\n");
             check.add(s);
+            //count=1;
             return check;
         }
 
-        if(!bool[rowB-row][colB-col])
+        if(!bool[rowB-row][colB-col]) {
             return check;
+        }
 
         bool[rowB-row][colB-col]=false;
+
+        // Putting count at each step
+        path[rowB-row][colB-col]=count;
+
         ArrayList<String> pathDown = new ArrayList<>();
         ArrayList<String> pathRight = new ArrayList<>();
         ArrayList<String> pathUp = new ArrayList<>();
@@ -47,27 +60,28 @@ public class AllPaths {
         // For down
         if(row>1){
            // bool[rowB-row][colB-col]=false;
-            pathDown = helperFunction(row-1,col,bool,s+"D",rowB,colB);
+            pathDown = helperFunction(row-1,col,bool,s+"D",rowB,colB,count+1,path);
         }
         // For Right
         if(col>1){
            // bool[rowB-row][colB-col]=false;
-            pathRight = helperFunction(row,col-1,bool,s+"R",rowB,colB);
+            pathRight = helperFunction(row,col-1,bool,s+"R",rowB,colB,count+1,path);
         }
         // For Up
-        if(row<3){
+        if(row<rowB){
             // bool[rowB-row][colB-col]=false;
-            pathUp = helperFunction(row+1,col,bool,s+"U",rowB,colB);
+            pathUp = helperFunction(row+1,col,bool,s+"U",rowB,colB,count+1,path);
         }
         // For Left
-        if(col<3){
+        if(col<colB){
             // bool[rowB-row][colB-col]=false;
-            pathLeft = helperFunction(row,col+1,bool,s+"L",rowB,colB);
+            pathLeft = helperFunction(row,col+1,bool,s+"L",rowB,colB,count+1,path);
         }
 
         // this is Backtracking
         // Now Here as we will be out of the recursive call at each level we will revert the changes
         bool[rowB-row][colB-col]=true;
+        path[rowB-row][colB-col]=0;
 
         check.addAll(pathDown);
         check.addAll(pathRight);
